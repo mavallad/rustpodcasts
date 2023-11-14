1ea3237a2c9c94e9ec314aaaa97d2e1112047448
Manuel Valladares <manuel.valladares@mirai.com>
2023-11-14 21:30:58 +0100

    More information in the pages. Add more podcasts. Upgrade shuttle

---
+++ shuttle/migrations/20231114173536_add_random_episodes.sql
@@ -0,0 +1,106 @@
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(16, 'Graphite - Raster and Vector Graphics in Rust - Keavon Chambers, Dennis Kobert', 'Keavon Chambers, Dennis Kobert',
+'✏️ In RustShip #4, Keavon Chambers and Dennis Kobert walk us through Graphite, a free and open-source raster and vector graphics editor written in Rust.
+
+👉 How much is Rust suitable for this domain? How to make an open-source project approachable for new contributors? What are the challenges of running a 2D graphic editor in the browser?',
+'en', 'https://podcasters.spotify.com/pod/show/marco-ieni',
+'2023-10-17', 101*60+37, null);
+
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(16, 'Corrode.dev and lychee - Matthias Endler', 'Matthias Endler',
+'🦀 In RustShip #5, Matthias Endler walks us through corrode.dev, his Rust consultancy company, and lychee, a fast, async, stream-based link checker written in Rust. 👉 Matthias will share with us what it''s like being a consultant and advice for developers who want to get into consultancy, especially for the Rust niche. 👉 Lychee is a tool you can run in your CI to check the links of your repositories. We will discover how lychee works and what pitfalls hide behind this task.',
+'en', 'https://podcasters.spotify.com/pod/show/marco-ieni',
+'2023-11-04', 86*60+29, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'rb-sys with Ian Ker-Seymer', 'Ian Ker-Seymer',
+'Allen Wyma talks with Ian Ker-Seymer about his work on rb-sys which easily allows you to integrate Ruby with Rust.',
+'en', 'https://rustacean-station.org/episode/ian-ker-sey/',
+'2023-09-28', 56*60+10, null);
+
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'What''s New in Rust 1.70 and 1.71', null,
+'Jon and Ben discuss the highlights of the 1.70 and 1.71 releases of Rust.',
+'en', 'https://rustacean-station.org/episode/rust-1.70-1.71/',
+'2023-09-30', 50*60+48, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Learn Rust in a Month of Lunches with Dave MacLeod', 'Dave MacLeod',
+'Allen Wyma talks with Dave MacLeod about his book “Learn Rust in a Month of Lunches” from Manning.',
+'en', 'https://rustacean-station.org/episode/dave-macleod/',
+'2023-10-05', 57*60+03, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Kraken''s migration to Rust microservices, with Rob Ede', 'Rob Ede',
+'Rob Ede, lead maintainer of Actix Web, explains to Marco Otto-Witte how (and why) Kraken chose to migrate their microservices to Rust.
+
+They also discuss Rust’s web development ecosystem at large, with a particular focus on Actix Web: Rob shares his view on how improvements
+in the language and framework space will eventually lead to a future where web development in Rust can be as approachable as
+web development in Javascript.',
+'en', 'https://rustacean-station.org/episode/rob-ede-kraken/',
+'2023-10-19', 32*60+13, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Announcing Rustacean Station and Rust 1.36', null,
+'Meet Rustacean Station, a new Rust “meta podcast”, and take a dive into the new 1.36.0 Rust release with Ben and Jon.',
+'en', 'https://rustacean-station.org/episode/000-rust-1.36.0/',
+'2019-07-12', 54*60+28, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Ruma and the Matrix Communication Protocol: An Interview with Jimmy Cuadra', 'Jimmy Cuadra',
+'We interview Jimmy Cuadra about Matrix, an open and decentralized communication protocol, and his implementation in Rust known as Ruma.',
+'en', 'https://rustacean-station.org/episode/001-ruma/',
+'2019-08-08', 63*60+29, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Organizing Colorado Gold Rust: An interview with conference founder J Haigh', 'J Haigh',
+'We interview J Haigh about their experience organizing this year’s first-ever Colorado Gold Rust conference, what brought them to Rust, and what inspired them to give back to Rust’s community.',
+'en', 'https://rustacean-station.org/episode/002-colorado-gold-rust/',
+'2019-08-25', 28*60+6, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(10, 'Fish Folk with Erlend Sogge Heggen', 'Erlend Sogge Heggen',
+'Allen Wyma talks with Erlend Sogge Heggen, Founder of Fish Folk. Fish Folk is a collection of open source games written in Rust.',
+'en', 'https://rustacean-station.org/episode/erlend-sogge-heggen/',
+'2023-06-26', 51*60+29, null);
+
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(6, 'Consulting, Web projects, Regression tests, TWIR, Rustacean Station, Meetups', null,
+'News about the latest consulting activities, podcasts, and latests developments in gitopolis',
+'en', 'https://share.transistor.fm/s/33eafe07',
+'2023-10-25', 21*60+36, null);
+
+insert into episodes
+(channel_id, title, guests, description, lang, url, date_published, duration_seconds, icon_path)
+values
+(5, '014 - Steve Klabnik', 'Steve Klabnik',
+'This episode is coming from the archives, originally recorded in May 2022 - James talks with Steve Klabnik from Oxide Computer
+about knowledge transfer within the Rust community, how learning-by-doing and reading datasheets help you develop, and
+how limits and regulations are put in place across many different fields.',
+'en', 'https://jamesmunns.com/podcast/014-steve/',
+'2023-09-29', 94*60+39, null);
